b2dc46d57a3c74b702487900df6f55266463d553
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-02-14 11:41:57 -0300

    commit

---
+++ acessoPonto.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Vitor",
+    idade: 20,
+    cpf: 11122233300,
+    email: "email@teste.com",
+};
+    //"." é um operador para acessar elementos individuais do objeto
+console.log(cliente.nome,/*cliente.idade, cliente.cpf, cliente.email*/)
