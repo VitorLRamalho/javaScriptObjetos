8c69aa955faaa3edb6e27643c6911222e6adb86e
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-02-14 11:34:10 -0300

    first commit

---
+++ objeto.js
@@ -0,0 +1,10 @@
+const listaDeCpf = [111,222,333];
+
+const informacoesPessoa = ['nome', 'Vitor', 'idade', 20, 'CPF', 11122233300];
+
+
+const objetoPessoa = {
+    nome: "Vitor",
+    idade:20,
+    
+}
