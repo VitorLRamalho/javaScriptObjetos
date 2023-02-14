c6e01c3cb0a8e7326915edf9ac2a54ec3e6c70b6
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-02-14 13:58:51 -0300

    commit

---
+++ tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Vitor",
+    idade: 20,
+    email: "Vitor@dominio.com",
+    telefone: [11911111111, 22922222222]
+  };
+
+  console.log(cliente.telefone/*[0], [1]*/);
