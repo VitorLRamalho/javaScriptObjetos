1f6fb14b4c68ff14ce7efde1bbc123989aab0384
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-02-14 15:15:06 -0300

    commit

---
+++ aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Vitor",
+    idade: 20,
+    email: "Vitor@dominio.com",
+    telefone: [11911111111, 22922222222]
+};
+
+cliente.endereço ={
+    rua: "Rua John Doe",
+    numero: 123,
+    apartamento: true,
+    complemento: "Apartamento 321",
+};
+
+console.log(cliente.endereço);
